a3f241e0e397b407d24ab438df298289c5044726
mikeykhalil <michael.khalil@dollarshaveclub.com>
2019-02-08 17:04:48 -0800

    check if gocql integration really breaks datadog apm

--- lib/tracing/tracing.go
+++ lib/tracing/tracing.go
@@ -1,7 +1,11 @@
 package tracing
 
 import (
+	"context"
+
 	"github.com/gocql/gocql"
+	gocqltrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/gocql/gocql"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/ext"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
 
@@ -17,12 +21,11 @@ func StartChildSpan(operationName string, parentSpan tracer.Span) tracer.Span {
 
 // GetTracedQuery will wrap a gocql Query and return a traced query with
 // some additional context from the parent.
-func GetTracedQuery(query *gocql.Query, parentSpan tracer.Span) *gocql.Query {
-	// _, ctx := tracer.StartSpanFromContext(context.Background(), datalayerOperation,
-	// 	tracer.SpanType(ext.SpanTypeCassandra),
-	// 	tracer.ChildOf(parentSpan.Context()),
-	// )
-	// tracedQuery := gocqltrace.WrapQuery(query, gocqltrace.WithServiceName("furan.gocql")).WithContext(ctx)
-	// return tracedQuery
-	return query
+func GetTracedQuery(query *gocql.Query, parentSpan tracer.Span) *gocqltrace.Query {
+	_, ctx := tracer.StartSpanFromContext(context.Background(), datalayerOperation,
+		tracer.SpanType(ext.SpanTypeCassandra),
+		tracer.ChildOf(parentSpan.Context()),
+	)
+	tracedQuery := gocqltrace.WrapQuery(query, gocqltrace.WithServiceName("furan-dqa.gocql")).WithContext(ctx)
+	return tracedQuery
 }
