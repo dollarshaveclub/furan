ffbdce8041f4c6155056f5b93ee07a1a2a686048
Benjamen Keroack <benjamen@dollarshaveclub.com>
2021-01-07 14:21:30 -0800

    circleci

--- testing/k8s/setup_cci_secrets.sh
+++ testing/k8s/setup_cci_secrets.sh
@@ -36,6 +36,6 @@ mkdir -p out
 sed -e "s/{VALUE}/$(echo -n ${GITHUB_TOKEN}|base64)/g" < ./github-token.yaml > ./out/k8s-github-token.yaml
 sed -e "s/{VALUE}/$(echo -n ${QUAY_TOKEN}|base64)/g" < ./quay-token.yaml > ./out/k8s-quay-token.yaml
 sed -e "s#{AKIDVALUE}#$(echo -n ${AWS_AKID}|base64)#g; s#{SAKVALUE}#$(echo -n ${AWS_SAK}|base64)#g" < ./aws-access-key.yaml > ./out/k8s-aws-access-key.yaml
-sed -e "s#{URIVALUE}#$(echo -n ${DB_URI}|base64)#g; s/{CEKVALUE}/$(echo -n ${DB_CEK}|base64)/g" < ./db.yaml > ./out/k8s-db.yaml
+sed -e "s#{URIVALUE}#$(echo -n ${DB_URI}|base64)#g; s#{CEKVALUE}#$(echo -n ${DB_CEK}|base64 -w0)#g" < ./db.yaml > ./out/k8s-db.yaml
 
 kubectl create -f ./out/
